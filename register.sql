6dcb72d426e6fc49d462937b3654889f9903da93
Gurkirat Singh <91982831+gurkirat20@users.noreply.github.com>
2022-06-13 15:22:26 +0530

    Delete register.sql

--- register.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `register` (
-  `ID` int(10) NOT NULL,
-  `User_Name` varchar(100) NOT NULL,
-  `Password` int(100) NOT NULL,
-  `Email` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
